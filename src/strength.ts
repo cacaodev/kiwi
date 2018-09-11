160d29189a311097b322bee2dc0f887db0a6a0dd
cacaodev <cacaodev@gmail.com>
2018-09-11 22:16:24 +0200

    Increase the number of symbolic strengths to 7, matching cocoa Autolayout default priorities.

--- src/strength.ts
+++ src/strength.ts
@@ -10,42 +10,49 @@
  * @class Strength
  */
  export class Strength {
-    /**
-     * Create a new symbolic strength.
-     *
-     * @param {Number} a strong
-     * @param {Number} b medium
-     * @param {Number} c weak
-     * @param {Number} [w] weight
-     * @return {Number} strength
-    */
-    static create( a: number, b: number, c: number, w: number = 1.0 ) {
-        let result: number = 0.0;
-        result += Math.max( 0.0, Math.min( 1000.0, a * w ) ) * 1000000.0;
-        result += Math.max( 0.0, Math.min( 1000.0, b * w ) ) * 1000.0;
-        result += Math.max( 0.0, Math.min( 1000.0, c * w ) );
-        return result;
-    }
+     /**
+      * Create a new symbolic strength.
+      *
+      * @param {Number} a a
+      * @param {Number} b b
+      * @param {Number} c c
+      * @param {Number} d d
+      * @param {Number} e e
+      * @param {Number} f f
+      * @param {Number} [w] weight
+      * @return {Number} strength
+      */
+     static create( a: number, b: number, c: number, d: number, e: number, f: number, w: number = 1.0 )
+     {
+         let result: number = 0.0;
+         result += Math.max( 0.0, Math.min( 1000.0, a * w ) ) * Math.pow(10, 15);
+         result += Math.max( 0.0, Math.min( 1000.0, b * w ) ) * Math.pow(10, 12);
+         result += Math.max( 0.0, Math.min( 1000.0, c * w ) ) * Math.pow(10, 9);
+         result += Math.max( 0.0, Math.min( 1000.0, d * w ) ) * Math.pow(10, 6);
+         result += Math.max( 0.0, Math.min( 1000.0, e * w ) ) * Math.pow(10, 3);
+         result += Math.max( 0.0, Math.min( 1000.0, f * w ) );
+         return result;
+     }
 
-    /**
-     * The 'required' symbolic strength.
-     */
-    static required = Strength.create( 1000.0, 1000.0, 1000.0 );
+     /**
+      * The 'required' symbolic strength.
+      */
+     static required = Strength.create( 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0 );
 
-    /**
-     * The 'strong' symbolic strength.
-     */
-    static strong = Strength.create( 1.0, 0.0, 0.0 );
+     /**
+      * The 'strong' symbolic strength.
+      */
+     static strong = Strength.create( 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 );
 
-    /**
-     * The 'medium' symbolic strength.
-     */
-    static medium = Strength.create( 0.0, 1.0, 0.0 );
+     /**
+      * The 'medium' symbolic strength.
+      */
+     static medium = Strength.create( 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 );
 
-    /**
-     * The 'weak' symbolic strength.
-     */
-    static weak = Strength.create( 0.0, 0.0, 1.0 );
+     /**
+      * The 'weak' symbolic strength.
+      */
+     static weak = Strength.create( 0.0, 0.0, 0.0, 0.0, 0.0, 1.0 );
 
     /**
      * Clip a symbolic strength to the allowed min and max.
